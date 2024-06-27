fe7fef5f22a9bb6f2e9e3d434c219a897b76653b
Fabio.Storer <fabiostorer19@gmail.com>
2024-06-27 19:07:09 -0300

    feat: adicionado pergunta sobre a historia e o comando split não funcional para contagem

---
+++ index.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+let historia = []
+
+historia = prompt('Olá. Estou aqui para ouvir uma história sua. Pode começar a digitar: ');
+
+historia.split( )
+
+console.log(historia.length)
